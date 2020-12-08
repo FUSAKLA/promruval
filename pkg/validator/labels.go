ac5b867e7e81aca4d868f29a839175ca1726bbec
bullywhippet <kellywdev@gmail.com>
2020-12-08 10:49:02 -0600

    fix typo

--- pkg/validator/labels.go
+++ pkg/validator/labels.go
@@ -134,7 +134,7 @@ func (h hasAnyOfLabels) Validate(rule rulefmt.Rule) []error {
 			return []error{}
 		}
 	}
-	return []error{fmt.Errorf("missing any of these annotatios `%s`", strings.Join(h.labels, "`,`"))}
+	return []error{fmt.Errorf("missing any of these annotations `%s`", strings.Join(h.labels, "`,`"))}
 }
 
 func newLabelHasAllowedValue(paramsConfig yaml.Node) (Validator, error) {
