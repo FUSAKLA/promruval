666b9c01aa272ff5957e7e316e7da868d40f2e11
Juraj Michálek <juraj.michalek132@gmail.com>
2024-10-14 22:33:59 +0200

    chore: allow whitespace after comma in ignore validations comments (#89)
    * chore: allow whitespace after comma in ignore validations comments
    
    * chore: remove empty elements caused by trailing comma
    
    * move exclude rules generation into a func and add tests
    
    * simplifed generateExcludedRules and updated test case per request
    
    * add deduplication of ignored rule names
    
    * remove unused field
    
    * avoid using continue in a loop
    
    * update unit tests to account for deduplication
    
    * fix: lint
    
    Signed-off-by: Martin Chodur <m.chodur@seznam.cz>
    
    * fix: white spaces in the `disabled_validation_rules` annotation CSV
    
    Signed-off-by: Martin Chodur <m.chodur@seznam.cz>
    
    * fix: assert import
    
    Signed-off-by: Martin Chodur <m.chodur@seznam.cz>
    
    ---------
    
    Signed-off-by: Martin Chodur <m.chodur@seznam.cz>
    Co-authored-by: Martin Chodur <m.chodur@seznam.cz>

---
+++ pkg/validate/validate_test.go
@@ -0,0 +1,62 @@
+package validate
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateExcludedRules(t *testing.T) {
+	type testCase struct {
+		input    string
+		expected []string
+	}
+	testCases := []testCase{
+		{
+			input:    "check-test-label,check-testy-label",
+			expected: []string{"check-test-label", "check-testy-label"},
+		},
+		{
+			input:    "check-test-label,check-test-label",
+			expected: []string{"check-test-label"},
+		},
+		{
+			input:    "check-test-label, check-testy-label",
+			expected: []string{"check-test-label", "check-testy-label"},
+		},
+		{
+			input:    "check-test-label, check-testy-label,",
+			expected: []string{"check-test-label", "check-testy-label"},
+		},
+		{
+			input:    "check-test-label ,check-test-label",
+			expected: []string{"check-test-label"},
+		},
+		{
+			input:    "check-test-label,check-test-label, ",
+			expected: []string{"check-test-label"},
+		},
+		{
+			input:    "check-test-label  ,  check testy label , ",
+			expected: []string{"check testy label", "check-test-label"},
+		},
+		{
+			input:    "check-test-label , check-testy-label    ",
+			expected: []string{"check-test-label", "check-testy-label"},
+		},
+		{
+			input:    "check-test-label , check-testy-label, ,    ",
+			expected: []string{"check-test-label", "check-testy-label"},
+		},
+		{
+			input:    " check-test-label , check-testy-label , ,    ",
+			expected: []string{"check-test-label", "check-testy-label"},
+		},
+	}
+	for i, tc := range testCases {
+		t.Run(string(rune(i)), func(t *testing.T) {
+			result := generateExcludedRules(tc.input)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
